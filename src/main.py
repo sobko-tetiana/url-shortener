e14fdc94b229af2015c8e115c81c059d4b34ff00
Tetiana Sobko <Tetiana.Sobko@zinkworks.com>
2026-09-22 11:47:45 +0100

    Include router in FastAPI application

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from src.routes import router
+
 app = FastAPI()
 
-@app.get("/")
+
+app.include_router(router)
+
+
+@app.get("/hello")
 def read_root():
     return {"Hello": "World"}
