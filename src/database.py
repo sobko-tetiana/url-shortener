9a462ceca32b4ee48ab265439779910a2647a2dd
Tetiana Sobko <Tetiana.Sobko@zinkworks.com>
2026-09-22 11:28:39 +0100

    Refactor import statements in database.py

--- src/database.py
+++ src/database.py
@@ -1,11 +1,11 @@
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
-    AsyncSession, async_sessionmaker
+    AsyncSession,
+    async_sessionmaker
 )
-from sqlalchemy.orm import DeclarativeBase
 
-from settings import get_settings
+from src.settings import get_settings
 
 settings = get_settings()
 engine = create_async_engine(settings.DATABASE_URL, echo=False)
