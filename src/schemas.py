a458f63486663848e2ea935aa60da78296e55e49
Tetiana Sobko <Tetiana.Sobko@zinkworks.com>
2026-09-23 10:24:16 +0100

    Add token refresh request and response schemas

--- src/schemas.py
+++ src/schemas.py
@@ -39,3 +39,12 @@ class UserLogoutRequestSchema(BaseModel):
 
 class MessageResponseSchema(BaseModel):
     message: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
